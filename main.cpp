f7e5e14fdab6b0bde43652eaf40c7030a8de4b2c
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2018-08-26 11:33:12 +0300

    Added some simple routines to read from a file into a OGG buffer.

--- main.cpp
+++ main.cpp
@@ -17,9 +17,40 @@
 #include <cstdio>
 #include <cstdlib>
 
+const auto BUFFER_SIZE = 4096;
+
+enum class State { STOPPED, STARTED };
+
+// ==========================================================================
+// A helper function to read data block from a file into the OGG container.
+// @param file The file to read from.
+// @param state The OGG synchronization state.
+// ==========================================================================
+static int readData(FILE& file, ogg_sync_state& state) {
+  auto buffer = ogg_sync_buffer(&state, BUFFER_SIZE);
+  auto bytes = fread(buffer, 1, BUFFER_SIZE, &file);
+  if (ogg_sync_wrote(&state, bytes) != 0) {
+    printf("ogg_sync_wrote failed: An internal error occured.\n");
+    exit(EXIT_FAILURE);
+  }
+  return bytes;
+}
+
+// ==========================================================================
+
 int main()
 {
   printf("%s\n", th_version_string());
+
+  // ==========================================================================
+  // OPEN THE TARGET OGG FILE
+  // open the target OGG file so we can start parsing it with our OGG parser.
+  // ==========================================================================
+  FILE* file = fopen("test.ogg", "rb");
+  if (file == nullptr) {
+    printf("fopen failed: Unable to locate test.ogg file.\n");
+    exit(EXIT_FAILURE);
+  }
 
   // init OGG sync state for data retrieval.
   ogg_sync_state oss;
@@ -41,7 +72,10 @@ int main()
   th_info ti;
   th_info_init(&ti);
 
+  auto state = State::STOPPED;
+  while (state == State::STOPPED) {
 
+  }
 
   // free the memory allocated for the header containers.
   th_info_clear(&ti);
@@ -49,5 +83,8 @@ int main()
 
   // free the OGG sync storage and reset to initial state.
   ogg_sync_clear(&oss);
+
+  // close the OGG file.
+  fclose(file);
   return EXIT_SUCCESS;
 }
